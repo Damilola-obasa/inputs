27c1fa412614e44f6ba9b8480724526bf88a1b0f
Micheal <damilolamobasa@gmail.com>
2025-08-26 12:43:17 -0400

    Fix script with debug output and error checking

--- .buildkite/gen_name.sh
+++ .buildkite/gen_name.sh
@@ -8,6 +8,8 @@ TIMESTAMP=$(date +%s)
 
 # Get current branch name (or use 'main' as default)
 BRANCH_NAME=${BUILDKITE_BRANCH:-main}
+echo "Debug: BUILDKITE_BRANCH=$BUILDKITE_BRANCH" >&2
+echo "Debug: BRANCH_NAME=$BRANCH_NAME" >&2
 
 # Generate a simple deploy target name
 echo "pex-${TIMESTAMP}-deploy-${BRANCH_NAME}"
